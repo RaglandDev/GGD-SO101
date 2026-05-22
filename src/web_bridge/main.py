dd29604d65a75dc34dda61ebfe2b73f9802291a3
Alexander Ragland <alexragland2003@gmail.com>
2026-05-21 20:57:09 -0700

    webcam streamer

--- src/web_bridge/main.py
+++ src/web_bridge/main.py
@@ -7,23 +7,27 @@ app = FastAPI()
 
 HTML_FILE_PATH = os.path.join(os.path.dirname(__file__), "index.html")
 
-# serve html 
 @app.get("/")
 async def get():
     with open(HTML_FILE_PATH, "r") as file:
         html_content = file.read()
     return HTMLResponse(html_content)
 
-# receive compressed frames
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     print("SUCCESS: Browser connected to Docker Backend!")
     try:
         while True:
-            data = await websocket.receive_text()
-            print(f"Received compressed frame: {len(data)} bytes")
-    except Exception:
+            frame_bytes = await websocket.receive_bytes() # raw JPEG buffer
+            
+            # ros2 publisher here
+
+            print(f"web_input_bridge-1  | Received compressed frame: {len(frame_bytes)} bytes", flush=True)
+            
+    except Exception as e:
+        print(e)
+    finally:
         print("Browser disconnected.")
 
 if __name__ == "__main__":
