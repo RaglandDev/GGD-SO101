b24cf44638516b7734c1f64c33e11c4d43a159e1
Alexander Ragland <alexragland2003@gmail.com>
2026-05-26 15:53:32 -0700

    perception setup

---
+++ src/perception/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/humble/setup.bash
+source /workspace/install/setup.bash
+
+ros2 run ros2_perception perception_node &
+
+cd /workspace
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+~                                                                                                                                                        
+"entrypoint.sh" 10L, 197B
+
